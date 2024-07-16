d07bae6cb9095e6bb920ea377a453e4e4f52a58d
Vignesh-mec <v8481263@gmail.com>
2024-07-16 03:52:43 -0100

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,7 @@
+from flask import Flask
+app=Flask(__name__)
+@app.route('/') 
+def hello_world():
+    return "<div><h1>Hello world</h1><br><h1>hai</h1>"
+if __name__=='__main__':
+     app.run(debug=True)
